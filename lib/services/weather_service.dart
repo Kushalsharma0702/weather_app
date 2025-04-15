2975598f4fcfc01a2c841e6fcc9e2fab2c356ae1
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2025-04-15 23:18:49 +0530

    add more features

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,16 +3,28 @@ import 'package:http/http.dart' as http;
 
 class WeatherService {
   static const String _apiKey = 'ceb7acd7ff0bed570c184ad38e092f42';
-  static const String _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  static const String _weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  static const String _forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
 
   static Future<Map<String, dynamic>> getWeather(String city) async {
-    final url = Uri.parse('$_baseUrl?q=$city&appid=$_apiKey&units=metric');
+    final url = Uri.parse('$_weatherUrl?q=$city&appid=$_apiKey&units=metric');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
     } else {
       throw Exception('Failed to load weather data');
+    }
+  }
+
+  static Future<Map<String, dynamic>> getForecast(String city) async {
+    final url = Uri.parse('$_forecastUrl?q=$city&appid=$_apiKey&units=metric');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load forecast data');
     }
   }
 }
